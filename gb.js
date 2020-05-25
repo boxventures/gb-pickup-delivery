83ed6723f471faed062435e4d3c04898a9c77dbb
Mikias Abera <itsmikias@gmail.com>
2020-05-25 10:57:32 -0400

    Create react component

---
+++ gb.js
@@ -0,0 +1,12 @@
+export default function GrocerBoxComponent() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(
+    <GrocerBoxComponent />,
+    document.getElementById('gb-component')
+);
